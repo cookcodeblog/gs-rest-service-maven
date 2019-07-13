b82e8c22943def6b7a54bc189db81434fe8c2948
cookcodeblog <williamsrlin@outlook.com>
2019-07-13 09:41:26 +0800

    Update docker build steps

--- docker-build.sh
+++ docker-build.sh
@@ -11,7 +11,7 @@ JAR_NAME=gs-rest-service
 JAR_VERSION=$1
 JAR_FILE=${JAR_NAME}-${JAR_VERSION}.jar
 
-MG_NAME=${JAR_NAME}
+IMG_NAME=${JAR_NAME}
 IMG_VERSION=${JAR_VERSION}
 docker build -t ${IMG_NAME}:${IMG_VERSION} --build-arg jar_file=${JAR_FILE} .
 
